50a755f8b08490f18c3f812b640e46c05de8f507
Will Ballard <wballard@mailframe.net>
2013-10-06 12:08:48 -0400

    basic status"

--- jobs/disk.job.js
+++
@@ -1,7 +0,0 @@
-
-var fs = require('fs');
-setInterval(function() {
-  var free = parseFloat(fs.readFileSync('/var/starphleet/diagnostic/free_disk'));
-  var used = 100 - free;
-  send_event('disk', {value: Math.floor(used)});
-}, 5 * 1000);
--- jobs/ram.job.js
+++
@@ -1,7 +0,0 @@
-
-var fs = require('fs');
-setInterval(function() {
-  var free = parseFloat(fs.readFileSync('/var/starphleet/diagnostic/free_ram'));
-  var used = 100 - free;
-  send_event('ram', {value: Math.floor(used)});
-}, 5 * 1000);
