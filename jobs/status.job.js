dfd0a9acb42a4ce6b39c4c566c8e10010d9f48d0
Will Ballard <wballard@mailframe.net>
2013-10-20 14:49:24 -0500

    sliding time window on CPU stats

--- jobs/status.job.js
+++ jobs/status.job.js
@@ -5,14 +5,16 @@ var moment = require('moment');
 require('shellscript').globalize();
 
 var cpu_points = [];
-for (var i = 1; i <= 10; i++) {
-  cpu_points.push({x: i, y: 0});
+var MAX_CPU_POINTS = 60;
+for (var i = 1; i <= MAX_CPU_POINTS; i++) {
+  cpu_points.push({x: Date.now()/1000, y: 0});
 }
 var last_x = cpu_points[cpu_points.length - 1].x;
 
 setInterval(function() {
   var status = yaml.safeLoad($('starphleet-status'));
-  cpu_points.push({x: ++last_x, y: Math.round(100 - status.free_cpu)});
+  cpu_points.push({x: Date.now()/1000, y: Math.round(100 - status.free_cpu)});
+  cpu_points.shift();
   send_event('cpu', {points: cpu_points});
   send_event('ram', {value: Math.round(100 - status.free_ram)});
   send_event('disk', {value: Math.round(100 - status.free_disk)});
